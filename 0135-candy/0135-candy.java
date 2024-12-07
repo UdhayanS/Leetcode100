76825100c7735ba278a3f086e746900785042952
Udhayan S <138105110+UdhayanS@users.noreply.github.com>
2024-12-07 14:30:53 +0530

    Time: 3 ms (83.57%), Space: 45.9 MB (15.69%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -2,9 +2,7 @@ class Solution {
     public int candy(int[] ratings) {
         int can[] = new int[ratings.length];
         int sum=0;
-        for(int i=0 ; i<ratings.length ; i++){
-            can[i] = 1;
-        }
+        Arrays.fill(can,1);
         for(int i=1; i<ratings.length; i++){
             if(ratings[i]>ratings[i-1]){
                 can[i]=can[i-1]+1;
