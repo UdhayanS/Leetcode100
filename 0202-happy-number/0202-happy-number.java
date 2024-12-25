6f3dced60b1fc39ce33197e894392aa5d59ef896
Udhayan S <138105110+UdhayanS@users.noreply.github.com>
2024-12-25 10:45:38 +0530

    Time: 0 ms (100%), Space: 40.9 MB (32.97%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,15 +1,22 @@
 class Solution {
-    public static int cal(int num){
-        
-        String nS = Integer.toString(num);
-        int ans=0;
-        for(int i=0; i<nS.length(); i++){
-            int temp = nS.charAt(i) - '0';
-            ans = ans+(int)Math.pow(temp,2);
-        }
-        if(ans>9) return cal(ans);
-        return ans;
+    public static int cal(int num) {
+    int ans = 0;
+    
+    // Process the digits using % and /
+    while (num > 0) {
+        int digit = num % 10; // Get the last digit
+        ans += digit * digit; // Add square of the digit
+        num /= 10; // Remove the last digit
     }
+    
+    // If the result is greater than 9, recursively process it
+    if (ans > 9) {
+        return cal(ans);
+    }
+    
+    return ans;
+}
+
     public boolean isHappy(int n) {
         
         int ans = cal(n);
