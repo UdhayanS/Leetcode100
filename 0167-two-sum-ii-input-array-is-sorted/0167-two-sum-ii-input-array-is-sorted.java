0024f8d2087a067f4a9e1617a4c987025c80aa19
Udhayan S <138105110+UdhayanS@users.noreply.github.com>
2025-01-29 09:05:03 -0800

    Time: 1 ms (99.76%), Space: 47.2 MB (64.85%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,35 +1,35 @@
-class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        for(int i=0; i<numbers.length-1; i++){
-            for(int j=i+1; j<numbers.length; j++){
-                if(numbers[i]+numbers[j]==target){
-                    return new int[]{i+1, j+1};
-                }
-            }
-        }
-        return new int[]{1, 1};
-    }
-}
-
-
 // class Solution {
 //     public int[] twoSum(int[] numbers, int target) {
-        
-//         int k=0;
-//         int left=0;
-//         int right=numbers.length-1;
-//         while(left<right){
-//             int ans = numbers[left]+numbers[right];
-//             if(ans==target){
-//                 return new int[]{left+1, right+1};
-//             }
-//             if(ans>target){
-//                 right--;
-//             }
-//             else{
-//                 left++;
+//         for(int i=0; i<numbers.length-1; i++){
+//             for(int j=i+1; j<numbers.length; j++){
+//                 if(numbers[i]+numbers[j]==target){
+//                     return new int[]{i+1, j+1};
+//                 }
 //             }
 //         }
-//         return new int[]{left+1, right+1};
+//         return new int[]{1, 1};
 //     }
 // }
+
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        
+        int k=0;
+        int left=0;
+        int right=numbers.length-1;
+        while(left<right){
+            int ans = numbers[left]+numbers[right];
+            if(ans==target){
+                return new int[]{left+1, right+1};
+            }
+            if(ans>target){
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+        return new int[]{left+1, right+1};
+    }
+}
