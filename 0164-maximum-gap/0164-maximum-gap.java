4cc1fc1a4b7b5dc55672b6c221231b6aa751ce8e
Udhayan S <138105110+UdhayanS@users.noreply.github.com>
2025-01-29 08:35:30 -0800

    Time: 40 ms (68.33%), Space: 61 MB (32.09%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if(nums.length<2) return 0;
+        Arrays.sort(nums);
+        int max = 0;
+        for(int i=0;i<nums.length-1; i++){
+            int ans = nums[i+1]-nums[i];
+            max = Math.max(max, ans);
+        }
+        return max;
+    }
+}
