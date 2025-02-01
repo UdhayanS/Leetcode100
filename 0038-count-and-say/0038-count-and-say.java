8b3e5f3106ffb5d1c54afcd36b280166a7cf7cf7
Udhayan S <138105110+UdhayanS@users.noreply.github.com>
2025-02-01 06:41:21 -0800

    Time: 5 ms (49.67%), Space: 41.6 MB (53.63%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static String cal(String str){
+        StringBuilder res = new StringBuilder();
+        int count = 1;
+        if(str.equals("1")) return "11";
+        int flag=0;
+        for(int i=0; i<str.length()-1; i++){
+            char ch = str.charAt(i);
+            if(ch==str.charAt(i+1)){
+                //res.append(count);
+                //res.append(ch);
+                count++;
+                flag=1;
+            }
+            else{
+                res.append(count);
+                res.append(ch);
+                count=1;
+            }
+            
+
+        }
+        
+        res.append(count);
+        res.append(str.charAt(str.length()-1));
+        return res.toString();
+   
+    }
+    public String countAndSay(int n) {
+        if(n==1) return "1";
+        String str = "1";
+        for(int i=0; i<n-1; i++){
+            str = cal(str);
+            System.out.println(str);
+        }
+        return str.toString();
+
+    }
+}
