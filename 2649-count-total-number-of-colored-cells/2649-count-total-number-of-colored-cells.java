fad47372f9ad0adfea92b0089559fd2d517d5db4
Udhayan S <138105110+UdhayanS@users.noreply.github.com>
2025-03-27 22:39:01 +0530

    Time: 7 ms (5.59%), Space: 40.9 MB (18.39%) - LeetHub

---
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long coloredCells(int n) {
+        long count=0;
+        long ind = 1;
+        for(int i=0; i<n; i++){
+            if(i==n-1){
+                count += ind;
+                ind=ind+2;
+            }
+            else{
+                count += ind*2;
+                ind = ind+2;
+            }
+            
+        }
+        System.out.println(ind);
+        return count;
+    }
+}
