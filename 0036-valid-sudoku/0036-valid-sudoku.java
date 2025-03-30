539946da145285242c3dbcf9a876d75aa9b9d974
Udhayan S <138105110+UdhayanS@users.noreply.github.com>
2025-03-30 19:58:21 +0530

    Time: 2 ms (77.62%), Space: 44.7 MB (49.57%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char current = board[i][j];
+                if (current != '.') {
+
+                    for (int b = 0; b < 9; b++) {
+                        if (b != j && board[i][b] == current) {
+                            return false;
+                        }
+                    }
+                    for (int a = 0; a < 9; a++) {
+                        if (i != a && board[a][j] == current) {
+                            return false;
+                        }
+                    }
+                    int start = (i/3 )*3;
+                    int end = (j/3 )*3;
+                    for(int x = start; x<start+3; x++){
+                        for(int y=end; y<end+3; y++){
+                            if(i!=x && j!=y && board[x][y]==current){
+                                return false;
+                            }
+                        }
+                    }
+                    
+                }
+                
+                
+            }
+        }
+        return true;
+    }
+}
