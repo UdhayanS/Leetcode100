37f354d3776ce3a5221930afbbea890bd5dbffd3
Udhayan S <138105110+UdhayanS@users.noreply.github.com>
2025-03-30 19:58:43 +0530

    Time: 2 ms (77.62%), Space: 44.5 MB (57.82%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,7 +4,6 @@ class Solution {
             for (int j = 0; j < 9; j++) {
                 char current = board[i][j];
                 if (current != '.') {
-
                     for (int b = 0; b < 9; b++) {
                         if (b != j && board[i][b] == current) {
                             return false;
@@ -15,19 +14,16 @@ class Solution {
                             return false;
                         }
                     }
-                    int start = (i/3 )*3;
-                    int end = (j/3 )*3;
-                    for(int x = start; x<start+3; x++){
-                        for(int y=end; y<end+3; y++){
-                            if(i!=x && j!=y && board[x][y]==current){
+                    int start = (i / 3) * 3;
+                    int end = (j / 3) * 3;
+                    for (int x = start; x < start + 3; x++) {
+                        for (int y = end; y < end + 3; y++) {
+                            if (i != x && j != y && board[x][y] == current) {
                                 return false;
                             }
                         }
                     }
-                    
                 }
-                
-                
             }
         }
         return true;
