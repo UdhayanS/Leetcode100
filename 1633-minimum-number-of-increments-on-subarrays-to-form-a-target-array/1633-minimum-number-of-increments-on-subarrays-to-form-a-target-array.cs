d2fd67154f2c317e462dfcf07e098309c2f87f14
Udhayan S <138105110+UdhayanS@users.noreply.github.com>
2025-10-30 15:23:15 +0530

    Time: 2 ms (100%), Space: 60.5 MB (33.33%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cs
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cs
@@ -1,9 +1,19 @@
 public class Solution {
     public int MinNumberOperations(int[] target) {
+        // int count = target[0];
+        // for(int i=1; i<target.Length; i++){
+        //     int sum = target[i]-target[i-1];
+        //     count += Math.Max(sum, 0);
+        // }
+        // return count;
+
         int count = target[0];
+        int max = target[0];
         for(int i=1; i<target.Length; i++){
-            int sum = target[i]-target[i-1];
-            count += Math.Max(sum, 0);
+            
+            if(target[i]>target[i-1]){
+                count += target[i]-target[i-1];
+            }
         }
         return count;
     }
