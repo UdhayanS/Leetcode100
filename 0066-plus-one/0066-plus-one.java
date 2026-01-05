12f0f1e18a57d9a225e9795060bf9c7127cb4a4f
Udhayan S <138105110+UdhayanS@users.noreply.github.com>
2026-01-05 21:31:27 +0530

    Time: 0 ms (100%), Space: 43.3 MB (67.11%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,10 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int i = digits.length-1;
-        if(digits.length == 1 && digits[i]!=9){
-            digits[0] = digits[0]+1;
-            return digits;
-        } 
+         
         while(i!=-1){
             if(digits[i]!=9){
                 digits[i] = digits[i]+1;
